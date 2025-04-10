58bc9868ea0cceb4fa79ec1cbb753e07598ef1e8
hazim <muhammadhazim@yahoo.com>
2025-04-10 14:35:16 +0800

    build site to docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/wedding/',
+  build: {
+    outDir: 'docs', // 👈 Important
+  },
   plugins: [react()],
    server: {
     host: true, // or '0.0.0.0'
