0ac6e2abdaa60dfd10139075b805296e976e7d53
hazim <muhammadhazim@yahoo.com>
2025-04-10 19:38:01 +0800

    change console log

--- src/code/content/content.tsx
+++ src/code/content/content.tsx
@@ -56,7 +56,7 @@ function Content() {
 
       // Handle click outside of the window
   useEffect(() => {
-   console.log("hii,", process.env.SUPABASE_KEY);
+   console.log("hii,", import.meta.env.VITE_API_KEY);
    
   }, []);
 
