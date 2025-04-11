2395a4d02a5472c51e2d3bd49efdf9fd7dda3968
mudon <muhammadhazim57@gmail.com>
2025-04-11 12:28:45 +0800

    change timer

--- src/code/component/countdown/countdown.tsx
+++ src/code/component/countdown/countdown.tsx
@@ -27,7 +27,7 @@ const CountdownTimer = () => {
 
   return (
     <div className="flex justify-evenly flex-wrap gap-x-20">
-      <div className="text-center text-[25px] default-font">
+      {/* <div className="text-center text-[2em] default-font">
         {days.toString().padStart(2, "0")}
         <span className="block text-xl custom-font-cinzel">Hari</span>
       </div>
@@ -42,6 +42,33 @@ const CountdownTimer = () => {
       <div className="text-center text-[25px] default-font">
         {seconds.toString().padStart(2, "0")}
         <span className="block text-xl custom-font-cinzel">Saat</span>
+      </div> */}
+
+      <div className="grid grid-flow-col gap-2 text-center auto-cols-max">
+        <div className="flex flex-col p-2 rounded-md custom-font-cinzel bg-[#c9a87b] p-2 shadow-md shadow-black/30">
+          <span className="countdown font-mono text-2xl">
+            <span style={{"--value":15} as React.CSSProperties} aria-live="polite">{days.toString().padStart(2, "0")}</span>
+          </span>
+          days
+        </div>
+        <div className="flex flex-col p-2 rounded-md custom-font-cinzel bg-[#c9a87b] p-2 shadow-md shadow-black/30">
+          <span className="countdown font-mono text-2xl">
+            <span style={{"--value":10} as React.CSSProperties} aria-live="polite">{hours.toString().padStart(2, "0")}</span>
+          </span>
+          hours
+        </div>
+        <div className="flex flex-col p-2 rounded-md custom-font-cinzel bg-[#c9a87b] p-2 shadow-md shadow-black/30">
+          <span className="countdown font-mono text-2xl">
+            <span style={{"--value":24} as React.CSSProperties } aria-live="polite">{minutes.toString().padStart(2, "0")}</span>
+          </span>
+          min
+        </div>
+        <div className="flex flex-col p-2 rounded-md custom-font-cinzel bg-[#c9a87b] p-2 shadow-md shadow-black/30">
+          <span className="countdown font-mono text-2xl">
+            <span style={{"--value":59} as React.CSSProperties } aria-live="polite">{seconds.toString().padStart(2, "0")}</span>
+          </span>
+          sec
+        </div>
       </div>
     </div>
   );
